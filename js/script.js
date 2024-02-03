2516746df4cffa3f9478123caf92f4ee1cbb001e
prince1killer <pranavkumar456n@outlook.com>
2024-02-03 18:27:30 +0530

    second update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
 let wname = document.querySelector(".home_des_des")
 let count = 0;
-let wnameCount = 0
-console.log(wname)
-let wnameList = ["development","design"]
+let wnameList = ["Developer","Designer","Blogger","Youtuber"]
 setInterval(()=>{
-    if(count%4 == 0||count==0){
-        wname.innerHTML = wnameList[wnameCount]
-        wnameCount++;
-    }
-    if(wnameCount > wnameList.length-1){
-        wnameCount = 0
+    
+        wname.innerHTML = wnameList[count]
+        count++;
+    if(count > wnameList.length-1){
+        count = 0
     }
     console.log(count)
-    count++;
-    if(count == 200){
-        count = 0;
-    }
-},1000)
+},4000)
 
 
 let ul = document.querySelector(".nav-list-items")
@@ -28,6 +21,4 @@ const togglemenu = () => {
     else if(ul.style.display == "flex"){
         ul.style.display = "none"
     }
-    console.log(ul)
-    // if()
 } 
